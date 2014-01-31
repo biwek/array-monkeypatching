6e22a539ecbf62704e583d6690e8ad5ea9bfef97
biwek <biweks@gmail.com>
2014-01-31 12:42:24 +0000

    added more tests to check for arguments

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -16,7 +16,12 @@ describe SubArray do
 
 	it "should return a product of 4 elements in an array" do
 		subarray.concat([1, 2, 3, 4])
-		expect(subarray.injecting{|sum, x| sum * x}).to eq(24)
+		expect(subarray.injecting{|product, v| product * v}).to eq(24)
+	end
+
+	it "should return a product of 4 elements in an array and an argument provided" do
+		subarray.concat([1, 2, 3, 4])
+		expect(subarray.injecting(5){|product, v| product * v}).to eq(120)
 	end
 
 	it "should return longest word in the array" do 
